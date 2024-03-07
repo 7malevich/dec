ec0b3729a0d17748fc3468807f0ec5c96430969e
neeMalevich <neemalevich@gmail.com>
2024-03-07 18:09:23 +0300

    страница /chastnym-klientam/oplata/

---
+++ chastnym-klientam/oplata/posledstviya-neoplaty/index.php
@@ -0,0 +1,36 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Последствия неоплаты");
+?>
+<? $APPLICATION->IncludeComponent("bitrix:breadcrumb", "breadcrumbs", array(
+    "PATH" => "",
+    "SITE_ID" => "ru",
+    "START_FROM" => "1",
+),
+    false
+); ?>
+<div class="container">
+    <h1 class="h1-marginBot h1-relative"><?= $APPLICATION->showTitle() ?></h1>
+</div>
+
+<section>
+    <div class="block section-pt0">
+        <div class="container text-block">
+
+
+            <? $APPLICATION->IncludeComponent(
+                "bitrix:main.include",
+                "",
+                array(
+                    "AREA_FILE_SHOW" => "file",
+                    "PATH" => DEFAULT_TEMPLATE_PATH . "/components/bitrix/main.include/inc_chastnym_klientam/oplata/posledstviya-neoplaty.php"
+                ),
+                false);
+            ?>
+
+
+        </div>
+    </div>
+</section>
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
